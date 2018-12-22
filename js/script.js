e8e0ab60e6b3b5116546f7c1ce4ec18a326cdea1
Smirnov Mikhail <45009420+SmirnovMiV@users.noreply.github.com>
2018-12-22 16:38:57 +0300

    Задание №23(v.2)
    1) Переделана анимация открытия модальной формы.
    2) Добавлена анимация при пустых полях.
    3) В полях формы установлены и стилизованы placeholder.
    4) Подключен localstorage для 2х полей.
    5) Добавлена интерактивная карта.

---
+++ js/script.js
@@ -0,0 +1,47 @@
+var button=document.querySelector(".search-form-button");
+var modal=document.querySelector(".search-form-modal");
+var form=modal.querySelector("form");
+var arrival=modal.querySelector("[name=date-arrival]");
+var departure=modal.querySelector("[name=date-departure]");
+var adults=modal.querySelector("[name=number-of-adults]");      
+var children=modal.querySelector("[name=number-of-children]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("adults");
+  storage = localStorage.getItem("children");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+modal.classList.add("modal-hidden");
+
+button.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  console.log("Нажатие кнопки");
+  modal.classList.toggle("modal-show");
+  modal.classList.remove("modal-error");
+  arrival.focus();
+
+  if (storage) {
+    adults.value = storage;
+    children.value = storage;
+  }
+});
+
+form.addEventListener("submit", function(evt) {
+  if (!arrival.value || !departure.value || !adults.value || !children.value) {
+    evt.preventDefault();
+    console.log("Нужно заполнить все поля");          
+    modal.classList.remove("modal-error");
+    modal.offsetWidth = modal.offsetWidth;
+    modal.classList.add("modal-error");        
+  } else {
+      if (isStorageSupport) {
+        localStorage.setItem("adults", adults.value);
+        localStorage.setItem("children", children.value);
+      }
+    }          
+});
